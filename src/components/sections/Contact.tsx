fd418a115e45c635d21f45bf4bfa0c593bd11f2b
Benjamin Wilk <wilkbenjamin757@gmail.com>
2026-05-20 17:13:10 +0200

    Kontaktformular Fix

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -141,9 +141,16 @@ export function Contact() {
       const formData = new FormData(e.currentTarget);
       formData.append('access_key', WEB3FORMS_ACCESS_KEY);
 
+      const object = Object.fromEntries(formData);
+      const json = JSON.stringify(object);
+
       const response = await fetch('https://api.web3forms.com/submit', {
         method: 'POST',
-        body: formData,
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        body: json,
       });
 
       const data = await response.json();
